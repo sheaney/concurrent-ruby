3560cb913d9a3387743b78babd95e56af652ed57
Lucas Allan Amorim <lucas.allan@gmail.com>
2014-05-09 22:06:56 -0700

    Creating ActorServer, RemoteActorProxy and RemoteActorRef

---
+++ lib/concurrent/actor/remote_actor_proxy.rb
@@ -0,0 +1,33 @@
+require 'concurrent/actors'
+require 'concurrent/actor/actor_server'
+require 'drb/drb'
+
+module Concurrent
+
+  class RemoteActorProxy
+
+    DEFAULT_HOST = ActorServer::DEFAULT_HOST
+    DEFAULT_PORT = ActorServer::DEFAULT_PORT
+
+    def initialize(opts = {})
+      @host = opts.fetch(:host, DEFAULT_HOST)
+      @port = opts.fetch(:port, DEFAULT_PORT)
+    end
+
+    def start
+      @server ||= DRbObject.new_with_uri("druby://#{@host}:#{@port}") # TODO - connection pool
+    end
+
+    def running?
+      ! @server.nil?
+    end
+
+    def stop
+      @server = nil
+    end
+
+    def send(remote_id, *message)
+      @server.post(remote_id, *message) if running?
+    end
+  end
+end
