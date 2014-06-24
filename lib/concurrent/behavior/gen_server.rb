366d80838120522bd0b9dbdeedb444f6bd9e0baf
Jerry D'Antonio <stumpjumper@gmail.com>
2014-06-24 09:04:31 -0400

    wip: Second spike of GenServer uses Actress-like envelope.

--- lib/concurrent/behavior/gen_server.rb
+++ lib/concurrent/behavior/gen_server.rb
@@ -1,3 +1,5 @@
+require 'concurrent/executor/single_thread_executor'
+
 module Concurrent
 
   # 1> c(kitty_gen_server).
@@ -26,6 +28,32 @@ module Concurrent
   # #=> "Cat Stevens" was set free.
   # ok
 
+
+
+  # call/cast methods may be called from any thread
+  # cast/call generate messages which must be queued up and processed in order
+  # which means that an object must exist as a target for the messages
+  # this is not inconsistent with Erlang since a Pid is an "object" with state (a mailbox and an ID)
+  # messages must be processed in the order they are received and must be processed one at a time
+  # in this respect an Erlang processes is similar to a Ruby thread that loops forever
+  #
+  # so we need to be able to handle two types of messages: 
+  # * cast messages return a value (ask)
+  # * call messahes do not (tell)
+  #
+  # tell: return nil and handle async
+  # ask: return an ivar and handle async
+  # ask!: block on the ivar value, raise an exception on timeout
+  #
+  # To process a cast or call:
+  # * create a message with the state, type, an IVar, etc.
+  # * enqueue the message
+  # * dequeue the message on the worker thread
+  # * call the appropriate subclass
+  # * update the internal process state
+  # * update the IVar with the result
+  # * NOTE: the IVar may be nil (cast)
+
   # http://www.erlang.org/doc/design_principles/gen_server_concepts.html
   # http://www.erlang.org/doc/man/gen_server.html
   # http://learnyousomeerlang.com/clients-and-servers
@@ -38,94 +66,86 @@ module Concurrent
 
     def start(*args)
       #NOTE: need a way to handle an options hash
-      #NOTE: state needs to be an Atomic reference
       #NOTE: may want to check self for implemented callback methods
-      result, state, *rest = self.init(*args)
-      if result == :ok
-        Process.new(self, state)
-      else
-        raise StandardError.new(state)
-      end
+      state = self.init(*args)
+      Process.new(self, state)
     end
     alias_method :start_link, :start
 
     private
 
+    Envelope = Struct.new(:message, :ivar, :process)
+
     class Process
 
-      def initialize(mod, state)
-        #NOTE: @state needs to be an Atomic reference
-        @mod = mod
+      def initialize(server, state)
+        @server = server
         @state = state
+        @executor = Concurrent::SingleThreadExecutor.new
       end
 
-      def call(*args)
-        #NOTE: must block
-        #NOTE: @state needs to be an Atomic reference
-        result, *opts = @mod.handle_call(@state, *args)
-        case result
-        when :reply
-          @state = opts[1]
-          opts[0]
-        when :noreply
-          @state = opts[0]
-          nil
-        when :stop
-          @state = opts.pop
-          @mod.terminate(opts.first, @state)
-          opts[1]
-        else
-          raise StandardError.new("#{result} is not a valid result")
-        end
+      def cast(message)
+        message(message, nil)
+      end
+      alias_method :tell :cast
+      alias_method :<<, :cast
+
+      def call(message, ivar = IVar.new)
+        message(message, ivar)
+      end
+      alias_method :ask :call
+
+      def call!(message, ivar = IVar.new)
+        call(message, ivar).value!
+      end
+      alias_method :ask! :call!
+
+      def message(message, ivar = nil)
+        @executor.post(Envelope.new(message, ivar, self), &process_envelope)
+        return ivar || self
       end
 
-      def cast(*args)
-        #NOTE: must NOT block
-        #NOTE: @state needs to be an Atomic reference
-        result, *opts = @mod.handle_cast(@state, *args)
-        case result
-        when :noreply
-          @state = opts[0]
-          nil
-        when :stop
-          @state = opts.pop
-          @mod.terminate(opts.first, @state)
-          opts[1]
-        else
-          raise StandardError.new("#{result} is not a valid result")
+      def shutdown
+        # ???
+      end
+
+      def kill
+        # ???
+      end
+
+      private
+
+      def process_envelope(envelope)
+        begin
+          if envelope.ivar
+            result, @state = @server.handle_cast(@state, *args)
+          else
+            @state = @server.handle_call(@state, *args)
+          end
+        rescue => ex
+          # supress
+        ensure
+          ivar.complete(ex.nil?, result, ex) if ivar
         end
       end
     end
 
     public
-    #protected
 
-    # Result = {ok,State} | {ok,State,Timeout} | {ok,State,hibernate}
     def init(*args)
       warn "[#{self}] Warning: undefined callback function init"
-      [:ok, nil] 
+      # Result = {ok,State} | {ok,State,Timeout} | {ok,State,hibernate}
+      nil
     end
 
     def handle_call(state, *args)
       warn "[#{self}] Warning: undefined callback function handle_call"
-      # {reply,Reply,NewState}
-      # {reply,Reply,NewState,Timeout}
-      # {reply,Reply,NewState,hibernate}
-      # {noreply,NewState}
-      # {noreply,NewState,Timeout}
-      # {noreply,NewState,hibernate}
-      # {stop,Reason,Reply,NewState}
-      # {stop,Reason,NewState}
-      [:noreply, state]
+      [nil, state]
     end
 
     def handle_cast(state, *args)
       warn "[#{self}] Warning: undefined callback function handle_cast"
-      # {noreply,NewState}
-      # {noreply,NewState,Timeout}
-      # {noreply,NewState,hibernate}
-      # {stop,Reason,NewState}
-      [:noreply, state]
+      state
     end
 
     def terminate(state, reason)
@@ -133,13 +153,34 @@ module Concurrent
       nil
     end
   end
+end
 
-  module Kitty
-    extend GenServer
+module Kitty
+  extend GenServer
 
-    def handle_call(state, *args)
-      puts 'Kitty::handle_call'
-      [:noreply, state]
-    end
+  Cat = Struct.new(:name, :color, :description)
+
+  def order_cat(name, color, description)
+    call([:order, name, color, description])
+  end
+
+  def return_cat(cat)
+    cast(cat)
+  end
+
+  def close_shop
+    terminate
+  end
+
+  protected
+
+  def init(*args)
+    [] # start with an array of zero cats
+  end
+
+  def handle_cast(state, *args)
+  end
+
+  def handle_call(state, *args)
   end
 end
